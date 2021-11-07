c52c90c4ca1fe20b095d0b045c32059b8b78f296
thereywob <81127870+thereywob@users.noreply.github.com>
2021-11-07 17:02:55 -0500

    Fix Navbar expand issue and add CSS
    Fixed the issue of expanding the navbar and adding additional styling to the navbar

--- home.php
+++ home.php
@@ -5,62 +5,58 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <!-- Trafficker CSS -->
 <link href="styles.css" rel="stylesheet">
   <title>OU Traffic</title>
-  <style>
-    .sticky {
-      position: fixed;
-      top: 0;
-      width: 100%;
-    }
-    .sticky + .content {
-      padding-top: 102px;
-    }
-  </style>
 </head>
 <body style="background-color: grey;">
 
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
-    <a class="navbar-brand" style="color:#b59a57; padding-left:10px">OU Traffic</a>
-    <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <!-- Begin Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" style="color:#b59a57"><b>OU Traffic</b></a>
+    <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" id="active" onClick="" href="#">Parking</a>
-        </li>
+    <div class="collapse navbar-collapse justify-content-lg-center" id="navbarNav">
+      <ul class="navbar-nav">
+        <!-- Filter Options label -->
+        <a class="navbar-brand" style="opacity:.75"><b>Filter Options:</b></a>
+        <!-- Parking Button -->
         <li class="nav-item">
-          <a class="nav-link" href="#">Dining</a>
+          <a class="nav-link active" href="#">Parking</a>
         </li>
+        <!-- Dining Button -->
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Dining</a>
+        </li>
+        <!-- Recreation Button -->
         <li class="nav-item">
           <a class="nav-link" href="#">Recreation</a>
         </li>
+        <!-- Vending Button -->
         <li class="nav-item">
-          <a class="nav-link" href="#">Vending</a>
+          <a class="nav-link active" href="#">Vending</a>
         </li>
+        <!-- Printers Button -->
         <li class="nav-item">
           <a class="nav-link" href="#">Printers</a>
         </li>
+        <!-- Study Areas Button -->
         <li class="nav-item">
           <a class="nav-link" href="#">Study Areas</a>
         </li>
       </ul>
     </div>
-  </nav>
-  <!-- End Navbar -->
-
+  </div>
+</nav>
+<!-- End Navbar -->
 
 
   <!-- Map -->
   <img src="images/ou-map-100-done.png" id="map">
   <!-- End Map -->
-
-
 
 
 
