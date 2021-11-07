231ebe75186f0cee96bd67e40ffc229b78b3051a
thereywob <81127870+thereywob@users.noreply.github.com>
2021-11-07 14:28:45 -0500

    Add bootstrap navbar and change background
    Add bootstrap navbar and background color and add CSS stylesheet

--- home.php
+++ home.php
@@ -6,7 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <title>OU Traffic</title>
+<link href="styles.css" >
+  <title>OU Trafficker</title>
   <style>
     .sticky {
       position: fixed;
@@ -18,20 +19,44 @@
     }
   </style>
 </head>
-<body>
+<body style="background-color: grey;">
 
-  <!-- Header with menu icon -->
-  <div class="container-fluid d-flex align-items-center" id="header">
-    <img src="images/hamburger-menu-icon.png" style="cursor:pointer" width="70" height="70" class="me-3" alt="Bootstrap">
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="header">
+    <a class="navbar-brand" href="#" style="color:#b59a57;">OU Trafficker</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#" style="color:#b59a57;">Parking</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color:#b59a57;">Dining</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" style="color:#b59a57;">Recreation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" style="color:#b59a57;">Vending</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" style="color:#b59a57;">Printers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" style="color:#b59a57;">Study Areas</a>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
 
 
 
 
   <!-- Map -->
-  <img src="images/ou-map-100%-done.png" id="map">
-
+  <img src="images/ou-map-100-done.png" id="map">
 
 
 
