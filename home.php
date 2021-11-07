30f3c78c8d69e812173ed5ca2890d477f10bfd56
thereywob <81127870+thereywob@users.noreply.github.com>
2021-11-07 14:39:39 -0500

    Fix sticky header

--- home.php
+++ home.php
@@ -6,8 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link href="styles.css" >
-  <title>OU Trafficker</title>
+<!-- Trafficker CSS -->
+<link href="styles.css" rel="stylesheet">
+  <title>OU Traffic</title>
   <style>
     .sticky {
       position: fixed;
@@ -21,8 +22,8 @@
 </head>
 <body style="background-color: grey;">
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="header">
-    <a class="navbar-brand" href="#" style="color:#b59a57;">OU Trafficker</a>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
+    <a class="navbar-brand" href="#" style="color:#b59a57;">OU Traffic</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -63,16 +64,6 @@
 
 
 
-  <script>
-  // Sticky header function
-  window.onscroll = function() {myFunction()};
-  var header = document.getElementById("header");
-  var sticky = header.offsetTop;
-
-  function myFunction() {
-      header.classList.add("sticky");
-  }
-  </script>
   <!-- Bootstrap JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
