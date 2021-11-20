a454c08b687b8212d8319a6b806a891a8b94d0f2
thereywob <81127870+thereywob@users.noreply.github.com>
2021-11-20 10:51:42 -0500

    Add sass

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <!-- Trafficker CSS -->
-<link href="styles.css" rel="stylesheet">
+<link href="./sass/main.css" rel="stylesheet">
   <title>OU Traffic</title>
 </head>
 <body style="background-color: grey;">
@@ -53,14 +53,53 @@
 </nav>
 <!-- End Navbar -->
 
+<!-- Begin Parking Lot Icons -->
+<div id="P1" class="circle">P1</div>
+<div id="P2" class="circle">P2</div>
+<div id="P3" class="circle">P3</div>
+<div id="P5" class="circle">P5</div>
+<div id="P9" class="circle">P9</div>
+
+<div id="P11" class="circle">P11</div>
+<div id="P12" class="circle">P12</div>
+<div id="P13" class="circle">P13</div>
+<div id="P16" class="circle">P16</div>
+<div id="P17" class="circle">P17</div>
+<div id="P18" class="circle">P18</div>
+
+<div id="P24" class="circle">P24</div>
+<div id="P26" class="circle">P26</div>
+<div id="P28" class="circle">P28</div>
+<div id="P29" class="circle">P29</div>
+
+<div id="P31" class="circle">P31</div>
+<div id="P32" class="circle">P32</div>
+<div id="P35" class="circle">P35</div>
+<div id="P36" class="circle">P36</div>
+<div id="P37" class="circle">P37</div>
+<div id="P38" class="circle">P38</div>
+<div id="P39" class="circle">P39</div>
+
+<div id="P41" class="circle">P41</div>
+<div id="P42" class="circle">P42</div>
+
+<div id="P51" class="circle">P51</div>
+<div id="P53" class="circle">P53</div>
+<div id="P55" class="circle">P55</div>
+<div id="P57" class="circle">P57</div>
+
+<div id="P61" class="circle">P61</div>
+<!-- End Parking Lot Icons -->
 
   <!-- Map -->
   <img src="images/ou-map-100-done.png" id="map">
   <!-- End Map -->
 
-
-
   <!-- Bootstrap JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
+<footer>
+    <script type="text/javascript" src="js/effects.js"></script>
+</footer>
 </html>
