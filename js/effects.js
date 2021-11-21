0b17a04231ec8745ff9dc0c2532ea1d4bfbb8e48
thereywob <81127870+thereywob@users.noreply.github.com>
2021-11-21 13:16:55 -0500

    Test JS and CSS for modal

--- js/effects.js
+++ js/effects.js
@@ -1,11 +1,8 @@
 // Get the modal
-
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-
 var btn = document.getElementsByClassName("circle");
-
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -13,9 +10,22 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 for (let i = 0; i < 29; i++){
   btn[i].onclick = function(){
+    //var id = String(document.getElementByClassName("circle")[i]);
+    alert(document.getElementById(btn[i]));
     modal.style.display = "block";
   }
 }
+
+
+
+// function getID(){
+//   for (let i = 0; i < 29; i++){
+//     btn[i].onclick = function(){
+//       modal.style.display = "block";
+//       return i;
+//     }
+//   }
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
