c92d3f2b107b30f720e7c48bb76cebf122260069
thereywob <81127870+thereywob@users.noreply.github.com>
2021-11-23 14:15:03 -0500

    Seperate navbar file

--- home.php
+++ home.php
@@ -13,50 +13,9 @@
 </head>
 <body style="background-color: grey;">
 
-<?php include 'navbar.php' ?>
-  <!-- Begin Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" style="color:#b59a57"><b>OU Traffic</b></a>
-    <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-lg-center" id="navbarNav">
-      <ul class="navbar-nav">
-        <!-- Filter Options label -->
-        <a class="navbar-brand" style="opacity:.75"><b>Filter Options:</b></a>
-        <!-- Parking Button -->
-        <li class="nav-item">
-          <input type="submit" class="nav-link active">Parking</a>
-        </li>
-        <!-- Dining Button -->
-        <li class="nav-item">
-          <input type="submit" class="nav-link active">Dining</a>
-        </li>
-        <!-- Recreation Button -->
-        <li class="nav-item">
-          <input type="submit" class="nav-link active">Recreation</a>
-        </li>
-        <!-- Vending Button -->
-        <li class="nav-item">
-          <input type="submit" class="nav-link active">Vending</a>
-        </li>
-        <!-- Printers Button -->
-        <li class="nav-item">
-          <input type="submit" class="nav-link active">Printers</a>
-        </li>
-        <!-- Study Areas Button -->
-        <li class="nav-item">
-          <input type="submit" class="nav-link active">Study Areas</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<!-- End Navbar -->
-
 <!-- Display Parking Lot Icons -->
 <?php
+include "navbar.php";
 // Connect to database
 require "connect.php";
 // Select all columns from the parkinglots table in the database
--- navbar.php
+++ navbar.php
@@ -1,3 +1,9 @@
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+<!-- Trafficker CSS -->
+<link href="./sass/main.css" rel="stylesheet">
+
 <!-- Begin Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 <div class="container-fluid">
@@ -10,28 +16,46 @@
       <!-- Filter Options label -->
       <a class="navbar-brand" style="opacity:.75"><b>Filter Options:</b></a>
       <!-- Parking Button -->
+      <li class="checkboxNavBar">
+        <input type="checkbox">
+      </li>
       <li class="nav-item">
-        <input type="submit" class="nav-link active">Parking</a>
+        <a class="nav-link active">Parking</a>
       </li>
       <!-- Dining Button -->
+      <li class="checkboxNavBar">
+        <input type="checkbox">
+      </li>
       <li class="nav-item">
-        <input type="submit" class="nav-link active">Dining</a>
+        <a class="nav-link">Dining</a>
       </li>
       <!-- Recreation Button -->
+      <li class="checkboxNavBar">
+        <input type="checkbox">
+      </li>
       <li class="nav-item">
-        <input type="submit" class="nav-link active">Recreation</a>
+        <a class="nav-link">Recreation</a>
       </li>
       <!-- Vending Button -->
+      <li class="checkboxNavBar">
+        <input type="checkbox">
+      </li>
       <li class="nav-item">
-        <input type="submit" class="nav-link active">Vending</a>
+        <a class="nav-link">Vending</a>
       </li>
       <!-- Printers Button -->
+      <li class="checkboxNavBar">
+        <input type="checkbox">
+      </li>
       <li class="nav-item">
-        <input type="submit" class="nav-link active">Printers</a>
+        <a class="nav-link">Printers</a>
       </li>
       <!-- Study Areas Button -->
+      <li class="checkboxNavBar">
+        <input type="checkbox">
+      </li>
       <li class="nav-item">
-        <input type="submit" class="nav-link active">Study Areas</a>
+        <a class="nav-link">Study Areas</a>
       </li>
     </ul>
   </div>
