a124f1c1772606f21cc1527c6f90c91ea9a98264
jayfrankina <jay.frankina@yahoo.com>
2021-11-23 23:12:44 -0500

    Revert "Update filterSelection.php"
    This reverts commit 13b5c95b188a1d7b1e1a56ce3b9b92e07e5d277f.

--- filterSelection.php
+++ filterSelection.php
@@ -71,44 +71,8 @@ if ($parking == true) {
 }
 }
 */
- $connect -> close();
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
 ?>
-
-
-<!-- Begin Modal Popup -->
-<div id="myModal" class="modal">
-  <div class="modal-content">
-    <!-- Modal Header -->
-    <div class="modal-header">
-      <h1 class="modal-title" id="lotName">Example: Parking Lot P5</h1>
-      <button type="button" class="btn-close"></button>
-    </div>
-    <!-- Modal body -->
-    <div class="modal-body" id="modalBody">
-      <h3 id="busyLevel">Example: BUSY</h3>
-      <h6 id="busyReports">Example: 4 reports in the last hour</h6>
-      <p id="lotOvernight" style="display:inline">Example: No Overnight Parking</p>
-      <i id=overnightIcon></i>
-    </div>
-    <!-- Modal footer -->
-    <div class="modal-footer">
-      <button type="button" class="btn btn-danger">Report as Busy</button>
-      <button type="button" class="btn btn-secondary">Close</button>
-    </div>
-  </div>
-</div>
-<!-- End Modal Popup -->
-
-  <!-- Map -->
-  <img src="images/ou-map-100-done.png" id="map">
-  <!-- End Map -->
-
-
-  <!-- Bootstrap JavaScript -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-<footer>
-    <!-- Modal JS -->
-    <script type="text/javascript" src="js/effects.js"></script>
-</footer>
-</html>
