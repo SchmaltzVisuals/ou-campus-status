3be4ccd55ae95e256cb8eae066d5dfc34693c3be
thereywob <81127870+thereywob@users.noreply.github.com>
2021-11-28 19:45:01 -0500

    Combine navbar and filter back into home

--- home.php
+++ home.php
@@ -14,12 +14,74 @@
 </head>
 <body style="background-color: grey;">
 
-<!-- Display Parking Lot Icons -->
+  <!-- Begin Navbar -->
+  <form action="#" method="post">
+  <nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" style="color:#b59a57"><b>OU Traffic</b></a>
+    <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-lg-center" id="navbarNav">
+      <ul class="navbar-nav">
+        <!-- Filter Options label -->
+        <a class="navbar-brand" style="opacity:.75"><b>Filter Options:</b></a>
+        <!-- Parking Button -->
+        <li class="checkboxNavBar">
+          <input type="checkbox" class="checkboxNavBar" name="parking" id="parkingCB" value="true">
+          <label class="label" for="parkingCB" > Parking &nbsp;</label>
+        </li>
+        <!-- Dining Button -->
+        <li class="checkboxNavBar">
+          <input type="checkbox" class="checkboxNavBar" name="dining" id="diningCB" value="true">
+          <label class="label" for="diningCB" > Dining &nbsp;</label>
+        </li>
+        <!-- Recreation Button -->
+        <li class="checkboxNavBar">
+          <input type="checkbox" class="checkboxNavBar" name="recreation" id="recreationCB" value="true">
+          <label class="label" for="recreationCB" > Recreation &nbsp;</label>
+        </li>
+        <!-- Vending Button -->
+        <li class="checkboxNavBar">
+          <input type="checkbox" class="checkboxNavBar" name="vending" id="vendingCB" value="true">
+          <label class="label" for="vendingCB" > Vending &nbsp;</label>
+        </li>
+        <!-- Printers Button -->
+        <li class="checkboxNavBar">
+          <input type="checkbox" class="checkboxNavBar" name="printers" id="printersCB" value="true">
+          <label class="label" for="printersCB" >Printers &nbsp;</label>
+        </li>
+        <!-- Study Areas Button -->
+        <li class="checkboxNavBar">
+          <input type="checkbox" class="checkboxNavBar" name="studyAreas" id="studyAreasCB" value="true">
+          <label class="label" for="studyAreasCB" > Study Areas &nbsp;</label>
+        </li>
+        <br>
+        <li>
+          <input type="submit" name="submit" value="Apply Filter">
+        </li>
+      </ul>
+    </div>
+  </div>
+  </nav>
+  </form>
+  <!-- End Navbar -->
+
+<!-- Display Navbar -->
 <?php
-include "navbar.php";
-/*
-// Connect to database
-require "connect.php";
+session_start();
+require 'connect.php';
+$parking = $_POST['parking'];
+$dining = $_POST['dining'];
+$recreation = $_POST['recreation'];
+$vending = $_POST['vending'];
+$printers = $_POST['printers'];
+$studyAreas = $_POST['studyAreas'];
+
+
+
+// Begin parking lot filter
+if ($parking == true) {
 // Select all columns from the parkinglots table in the database
 $sql = "select * from parkinglots";
 $result = $connect->query($sql);
@@ -46,14 +108,43 @@ while ($row = $result->fetch_assoc()){
   } else {
     $overnightParking = "overnight-no";
   }
-
   // Print lot icon with these classes ("circle overnight-yes/no")
   echo '<button id="P'.$lotID.'" class="circle '.$overnightParking.' '.$numReports.' '.$lotIconColor.'" style="background-color:'.$lotIconColor.'">P'.$lotID.'</button>';
 }
-// Close the database connection
-$connect->close();
- */?>
-<!-- End Parking Lot Icons -->
+}
+// End parking lot filter
+
+// Begin dining filter
+if ($dining == true) {
+// Select all columns from the dining table in the database
+$sql = "select * from dining";
+$result = $connect->query($sql);
+// Loop through all rows in the table
+while ($row = $result->fetch_assoc()){
+  // Save the parking lot number and number of reports as variables
+  $numReports = $row['reports'];
+  $diningID = $row['id'];
+  // If the dining location has 20 or more reports
+  if ($numReports >= 20) {
+    $diningIconColor = "red";
+  }
+  // If the dining location has 10 or more (but less than 20) reports
+  else if ($numReports >= 10) {
+    $diningIconColor = "orange";
+  }
+  // If the dining location has less than 10 reports
+  else {
+    $diningIconColor = "lightgreen";
+  }
+  // Print dining icon
+  echo '<button class="diningCircle" id="Dining'.$diningID.'" style="background-color:'.$diningIconColor.'"><img src="images/dining.png" class="diningIcon"></button>';
+}
+}
+// End dining filter
+
+$connect -> close();
+?>
+<!-- End Navbar -->
 
 <!-- Begin Modal Popup -->
 <div id="myModal" class="modal">
