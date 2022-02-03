aa35cd2599391cb08ad3c8dca752d4d6ffa2f3f0
thereywob <81127870+thereywob@users.noreply.github.com>
2022-02-03 12:40:38 -0500

    Hide credentials
    Hide the database connection credentials for security

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 // Script to open a connection to the database
 // Connection details
-$hostname = "localhost";
-$username = "root";
-$password = "Bones123";
-$db_name = "ou_status";
+$hostname = "xxx";
+$username = "xxx";
+$password = "xxx";
+$db_name = "xxx";
 // Connect to mySQL
 $connect = new mysqli($hostname, $username, $password, $db_name);
 // Check connection
